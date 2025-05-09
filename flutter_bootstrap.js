27ad11d4345230ace74380bfa5e14fa6a35791f2
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-05-09 15:56:57 +0000

    deploy: 5aaafe6dbddbff51d10bd52dfb6f514a4651a95a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3014104258"
+    serviceWorkerVersion: "3581478324"
   }
 });
